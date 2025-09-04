72d848f0763c20959e55f25a7a323ebde068defb
Nivas Kumar S <nivask457@gmail.com>
2025-09-04 10:41:34 +0530

    File Updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import ShowSchools from "./showSchools";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  <ShowSchools/>
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
